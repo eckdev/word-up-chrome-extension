bb39e36b7103d76948124f987db857d381111b1b
Eyüp Can Kayadarçin <eck@Eyup-MacBook-Pro.local>
2023-06-16 19:05:04 +0300

    added word api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,68 @@
-import { Box } from "./styled";
+import { useEffect, useState } from "react";
+import {
+  Box,
+  DateStyled,
+  Pronounciation,
+  Word,
+  Wrapper,
+  PhoneticAudio,
+  Phonetic,
+  PosBlocks,
+  Pos,
+  Definition,
+} from "./styled";
+import { generate } from "random-words";
+import WordInfo from "./types";
 
 function App() {
+  const [wordInfo, setWordInfo] = useState<WordInfo | null>(null);
+  useEffect(() => {
+    const word = generate();
+    getWord(word);
+  }, []);
+
+  const getWord = async (word: string[]) => {
+    const result = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    ).then((res) => res.json());
+    setWordInfo(result[0]);
+  };
+
+  const playAudio = () => {
+    const audio = new Audio(wordInfo?.phonetics[0]?.audio);
+    if (wordInfo?.phonetics[0]?.audio) {
+      audio.play();
+    }
+  };
+
+  const getFormattedDate = () => {
+    const date = new Date(); // Geçerli tarih ve saat
+    const options: Intl.DateTimeFormatOptions = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' };
+    const formatter = new Intl.DateTimeFormat('en-US', options);
+    const formattedDate = formatter.format(date);
+    return formattedDate;
+  };
+
   return (
-    <Box>
-      <header>
-        <h5 style={{color: 'red'}}>Hello From React App</h5>
-      </header>
-    </Box>
+    <Wrapper>
+      <Box>
+        <DateStyled>{getFormattedDate()}</DateStyled>
+        <Word>{wordInfo?.word}</Word>
+        <Pronounciation>
+          <Phonetic>{wordInfo?.phonetic}</Phonetic>
+          <PhoneticAudio onClick={playAudio} />
+        </Pronounciation>
+        {wordInfo?.meanings &&
+          wordInfo?.meanings.map((item, index) => (
+            <PosBlocks key={index}>
+              <Pos>{item.partOfSpeech}</Pos>
+              {item.definitions.slice(0, 3).map((def,index) => (
+                <Definition key={index}>{def.definition}</Definition>
+              ))}
+            </PosBlocks>
+          ))}
+      </Box>
+    </Wrapper>
   );
 }
 
--- src/styled.ts
+++ src/styled.ts
@@ -1,10 +1,82 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 400px;
+  background: #fff;
+  border: 12px solid #00125d;
+  z-index: 999;
+  font-family: Arial, sans-serif;
+`;
 
 export const Box = styled.div`
-left: 0;
-top: 0;
-width: 300px;
-background: #2e9fff;
-border: 5px solid #c2c2c2;
-z-index: 999;
-`
+  padding: 56px 48px;
+  position: relative;
+`;
+
+export const DateStyled = styled.div`
+  margin-bottom: 16px;
+  font-size: 15px;
+  font-weight: 700;
+  text-transform: uppercase;
+`;
+
+export const Word = styled.h1`
+  display: inline-block;
+  font-size: 48px;
+  font-weight: 700;
+  white-space: nowrap;
+  color: #00125d;
+  margin-top: 0;
+  margin-bottom: 8px;
+`;
+
+export const Pronounciation = styled.div`
+  position: relative;
+  padding-bottom: 18px;
+  margin-bottom: 10px;
+
+  &:after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 38px;
+    border-bottom: 4px solid #d8d8d8;
+  }
+`;
+
+export const Phonetic = styled.span`
+  font-size: 20px;
+  margin-right: 8px;
+`;
+
+export const PhoneticAudio = styled.div`
+  display: inline-block;
+  vertical-align: top;
+  width: 24px;
+  height: 24px;
+  margin: 0 !important;
+  background: url("https://www.dictionary.com/e/wp-content/themes/dictionary-com/images/ic_volume_up.png")
+    no-repeat 0 0;
+  background-size: contain;
+  cursor: pointer;
+`;
+
+export const PosBlocks = styled.div`
+  margin-bottom: 12px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Pos = styled.span`
+  display: inline-block;
+  font-style: italic;
+  margin-bottom: 8px;
+  line-height: 1.1;
+`;
+
+export const Definition = styled.span`
+  display: inline-block;
+  margin-bottom: 4px;
+  line-height: 20px;
+`;
